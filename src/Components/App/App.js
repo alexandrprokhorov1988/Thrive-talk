95ba758f2ba176963714d02ec746d01f17ca3a35
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2021-01-14 01:09:09 +0300

    init cra

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+     <main className="content">
+       
+     </main>
+    </div>
+  );
+}
+
+export default App;
